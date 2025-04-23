f2d179bb9bd7db9a9f84f58de062e9d80a92fa9f
Margareth Ortiz <kamil.n24@gmail.com>
2025-04-23 15:45:04 -0500

    text changed for modal appointment

--- src/components/DoctorCard.tsx
+++ src/components/DoctorCard.tsx
@@ -256,7 +256,7 @@ const DoctorCard: React.FC<DoctorCardProps> = ({ doctor }) => {
                     <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
-                    Book Appointment with Dr. {doctor.name}
+                    Book 
                   </button>
                 </div>
               </div>
